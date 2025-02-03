4c47470d8c951b1ec18580304e9de83727459617
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-02-03 22:51:50 +0100

    Update packages (#118)
    # Update packages
    
    ## :recycle: Current situation & Problem
    Some packages are outdated
    
    
    ## :gear: Release Notes 
    * Update packages
    
    
    ### Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {},
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Spezi Web Design System open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+import { type Preview } from "@storybook/react";
+import "../modules/globals.css";
+/**
+ * We need to import preflight in Stories,
+ * because that's how components are required to be consumed in client-land
+ * */
+import "./preflight.css";
+
+const preview: Preview = {
+  parameters: {
+    layout: "centered",
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export default preview;
