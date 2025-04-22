77fb1f8ca6c9c478274f074cdf10bcac674d3f52
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2025-04-22 21:03:11 +0200

    Unify file header in the medication.ts file (#143)
    # Unify file header in the medication.ts file
    
    ## :recycle: Current situation & Problem
    `medication.ts` was inconsistent with rest of the codebase.
    
    
    ## :gear: Release Notes
    * Unify file header in the medication.ts file
    
    
    ### Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).

--- modules/firebase/medication.ts
+++ modules/firebase/medication.ts
@@ -1,7 +1,7 @@
+///
+// This source file is part of the Stanford Biodesign Digital Health ENGAGE-HF open-source project
 //
-// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
-//
-// SPDX-FileCopyrightText: 2023 Stanford University
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
